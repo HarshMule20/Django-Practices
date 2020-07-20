eb8a2b070171a763da0e400ddc7df977261a208b
Harsh Mule <hmuley22@gmail.com>
2020-07-21 00:07:45 +0530

    registration function

--- registration_form/models.py
+++ registration_form/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Register(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    profile_photo = models.TextField()
+    gender = models.CharField(max_length=10)
+    phone_no = models.CharField(max_length=10)
+    address_1 = models.TextField()
+    address_2 = models.TextField()
+    dob = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    pin_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
--- registration_form/views.py
+++ registration_form/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from registration_form.models import Register
+
+
+def registration_form(request):
+    # return HttpResponse("<h1> Hello world </h1>")
+    # return render(request, 'registration_form/index.html')
+    if request.method == 'POST':
+        print("in registration")
+        name = request.POST.get('name', '')
+        surname = request.POST.get('surname', '')
+        profile_photo = request.POST.get('profile_photo', '')
+        email = request.POST.get('email', '')
+        gender = request.POST.get('gender', '')
+        phone_no = request.POST.get('phone', '')
+        address_1 = request.POST.get('address_1', '')
+        address_2 = request.POST.get('address_2', '')
+        dob = request.POST.get('dob', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        pin_code = request.POST.get('pin_code', '')
+        user_register = Register(
+            name=name,
+            surname=surname,
+            email=email,
+            profile_photo=profile_photo,
+            gender=gender,
+            phone_no=phone_no,
+            address_1=address_1,
+            address_2=address_2,
+            dob=dob,
+            city=city,
+            state=state,
+            pin_code=pin_code,
+        )
+        user_register.save()
+        done = True
+        return render(request, 'registration_form/index.html', {'done': done})
+    return render(request, 'registration_form/index.html')
